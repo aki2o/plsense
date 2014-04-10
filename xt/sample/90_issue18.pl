05ff6ebe738e586c9767659862223ca902aa65bd
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2014-04-10 10:37:31 +0900

    add test for #21

--- xt/sample/90_issue18.pl
+++ xt/sample/90_issue18.pl
@@ -6,3 +6,19 @@ use File::Copy;
 #&File::Copy::
 # aend equal: File::Copy::dummy_copy
 
+# mstart project module prior method
+#File::Copy::dummy_copy
+# mend ^ NAME: \s dummy_copy $
+# mend ^ RETURN: \s Literal \s As \s SCALAR $
+# mend ^ FILE: \s .+ /File\.Copy\.pm $
+# mend ^ LINE: \s 6 $
+# mend ^ COL: \s 1 $
+
+# mstart explicit project module prior method
+#&File::Copy::dummy_copy
+# mend ^ NAME: \s dummy_copy $
+# mend ^ RETURN: \s Literal \s As \s SCALAR $
+# mend ^ FILE: \s .+ /File\.Copy\.pm $
+# mend ^ LINE: \s 6 $
+# mend ^ COL: \s 1 $
+
